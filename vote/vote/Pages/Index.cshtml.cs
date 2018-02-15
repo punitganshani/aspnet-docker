5840826713e361d24918f7d04d47e5d5339ac029
Puneet Ghanshani <pugh@microsoft.com>
2018-02-15 22:02:15 +0900

    Initial Commit - Basic Docker with Voting

---
+++ vote/vote/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace vote.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string HostName { get; set; } = Environment.MachineName;
+        public string OptionA { get; set; } = "Tabs";
+        public string OptionB { get; set; } = "Spaces";
+
+
+        [BindProperty]
+        public string Vote { get; set; }
+
+        public async Task OnGetAsync()
+        {
+        }
+
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> OnPostAsync()
+        {
+            return Page();
+        }
+
+    }
+}
